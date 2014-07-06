616bda7d6fccedc15e6afc39e56c618bb3aecb59
gianasista <gianasista@users.sourceforge.net>
2014-07-06 21:57:07 +0200

    custom app launcher icon

--- src/de/gianasista/tldr_viewer/CommandContentActivity.java
+++ src/de/gianasista/tldr_viewer/CommandContentActivity.java
@@ -2,27 +2,54 @@ package de.gianasista.tldr_viewer;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.internal.widget.ProgressBarICS;
+import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
+import android.view.ViewGroup;
+import android.view.ViewManager;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import de.gianasista.tldr_viewer.util.CommandContentDelegate;
 import de.gianasista.tldr_viewer.util.TldrContentProvider;
 
 /**
  * @author vera
  *
  */
-public class CommandContentActivity extends ActionBarActivity 
+public class CommandContentActivity extends ActionBarActivity implements CommandContentDelegate
 {
+	private TextView textView;
+	private TldrContentProvider contentProvider;
+	
+	ProgressBar progressBar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_command_content);
-		TextView textView = (TextView)findViewById(R.id.detail_content);
+		textView = (TextView)findViewById(R.id.detail_content);
 		String commandName = getIntent().getStringExtra(CommandListActivity.COMMAND_NAME);
 		getSupportActionBar().setTitle(commandName);
 		
-		textView.setText(new TldrContentProvider(getAssets()).getHtmlContentStringForCommand(commandName));
+		contentProvider = new TldrContentProvider(getAssets(), this);
+		contentProvider.loadHtmlContentStringForCommand(commandName);
 		textView.setMovementMethod(new ScrollingMovementMethod());
+		
+		if(progressBar == null)
+		{
+			progressBar = new ProgressBar(getApplicationContext());
+			addContentView(progressBar, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+		}
+		progressBar.setVisibility(ProgressBar.VISIBLE);
+	}
+
+	@Override
+	public void receiveCommandContent(String content) 
+	{
+		progressBar.setVisibility(ProgressBar.INVISIBLE);
+		textView.setText(Html.fromHtml(content));
 	}
 
 }
--- src/de/gianasista/tldr_viewer/util/TldrContentProvider.java
+++ src/de/gianasista/tldr_viewer/util/TldrContentProvider.java
@@ -1,16 +1,18 @@
-/**
- * 
- */
 package de.gianasista.tldr_viewer.util;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.InvalidObjectException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.text.Html;
 import android.text.Spanned;
 import android.util.Log;
@@ -20,29 +22,46 @@ import de.gianasista.tldr_viewer.CommandListActivity;
  * @author vera
  *
  */
-public class TldrContentProvider 
+public class TldrContentProvider extends AsyncTask<URL, Void, String>
 {
 	private AssetManager assetManager;
+	private CommandContentDelegate contentDelegate;
 	
 	public TldrContentProvider(AssetManager assetManager)
 	{
 		this.assetManager = assetManager;
 	}
 	
-	public String[] getCommandList()
+	public TldrContentProvider(AssetManager assetManager, CommandContentDelegate delegate)
 	{
-		String[] fileCommandList = {};
+		this(assetManager);
 		
-		try {
-			fileCommandList = assetManager.list("common");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			Log.e(CommandListActivity.class.getName(), e.getMessage());
-		}
-		
-		return stripFileExtensionFromList(fileCommandList);
+		this.contentDelegate = delegate;
 	}
 	
+	public String[] getCommandList()
+	{
+		List<String> resultList = new ArrayList<String>();
+		
+		try
+		{
+			InputStream inputStream = assetManager.open("common.txt");
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			String line;
+			
+			while( (line=bufferedReader.readLine()) != null)
+			{
+				resultList.add(line);
+			}
+		}
+		catch (IOException e) 
+		{
+			Log.e(this.getClass().getName(), e.getMessage());
+		}
+		
+		return stripFileExtensionFromList(resultList.toArray(new String[] {}));
+	}
+		
 	private String[] stripFileExtensionFromList(String[] listWithFileExtension)
 	{
 		List<String> resultList = new ArrayList<String>();
@@ -54,25 +73,62 @@ public class TldrContentProvider
 		return resultList.toArray(new String[] {});
 	}
 	
-	public Spanned getHtmlContentStringForCommand(String commandName)
+	public void loadHtmlContentStringForCommand(String commandName)
 	{
+		// https://raw.githubusercontent.com/tldr-pages/tldr/master/pages/common/ssh.md
+		try {
+			this.execute(new URL("https://raw.githubusercontent.com/tldr-pages/tldr/master/pages/common/"+commandName+".md"));
+		} catch (MalformedURLException e) {
+			Log.e(this.getClass().getName(), e.getMessage());
+		}
+	}
+
+	@Override
+	protected String doInBackground(URL... params) 
+	{
+		if(params.length == 0) return null;
+		
 		StringBuilder result = new StringBuilder();
 		MdFileContentParser parser = new MdFileContentParser();
+		DataInputStream urlDataStream = null;
+		BufferedReader reader = null;
 		
 		try {
-			InputStream inputStream = assetManager.open("common/"+commandName+".md");
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			urlDataStream = new DataInputStream(params[0].openStream());
+			reader = new BufferedReader(new InputStreamReader(urlDataStream));
 			String line;
 			
-			while( (line=bufferedReader.readLine()) != null)
+			while( (line=reader.readLine()) != null)
 			{
 				result.append(parser.parseLine(line));
 			}
 		} catch (IOException e) {
-			// TODO Close Stream
-			e.printStackTrace();
+			try 
+			{
+				if (reader != null) 
+					reader.close();
+			} catch(IOException exc) {
+				Log.e(this.getClass().getName(), exc.getMessage());
+			}
+			
+			try 
+			{
+				if (urlDataStream != null) 
+					urlDataStream.close();
+			} catch(IOException exc) {
+				Log.e(this.getClass().getName(), exc.getMessage());
+			}
+			
+			Log.e(this.getClass().getName(), e.getMessage());
 		}
 		
-		return Html.fromHtml(result.toString());
+		return result.toString();
+	}
+	
+	@Override
+	protected void onPostExecute(String result) 
+	{
+		super.onPostExecute(result);
+		contentDelegate.receiveCommandContent(result);
 	}
 }
